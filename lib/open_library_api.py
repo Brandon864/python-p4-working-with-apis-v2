9fc15c216da6a8eba877df349b6be2f6be3a62e9
Brandon864 <ojbrandon2345@mail.com>
2025-06-19 10:05:01 +0300

    Implement Open Library API integration

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -1,32 +1,23 @@
 import requests
 import json
 
-
 class Search:
-
     def get_search_results(self):
         search_term = "the lord of the rings"
-
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
         fields_formatted = ",".join(fields)
         limit = 1
-
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
         response = requests.get(URL)
         return response.content
 
     def get_search_results_json(self):
         search_term = "the lord of the rings"
-
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
         fields_formatted = ",".join(fields)
         limit = 1
-
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
         print(URL)
         response = requests.get(URL)
@@ -37,13 +28,10 @@ class Search:
         fields = ["title", "author_name"]
         fields_formatted = ",".join(fields)
         limit = 1
-
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
         response = requests.get(URL).json()
         response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
         return response_formatted
-
 
 # results = Search().get_search_results()
 # print(results)
